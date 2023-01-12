028b9898ea9a9249b11a6153766dc8caa076b8f4
Vladislav Serafimov <vladiserafimov@abv.bg>
2023-01-12 06:50:55 +0200

    Initial commit

---
+++ cmake-build-debug/CMakeFiles/Asign2.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "Asign2"
+  "Asign2.pdb"
+  "CMakeFiles/Asign2.dir/main.cpp.o"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Asign2.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
